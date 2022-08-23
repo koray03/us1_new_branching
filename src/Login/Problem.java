95493a83f496491bd3038e6331a60bdf8e5e3b63
Ethem Gokdeniz <koraygokdeniz@gmail.com>
2022-08-23 01:46:25 -0400

    problem class updated on master

--- src/Login/Problem.java
+++ src/Login/Problem.java
@@ -1,11 +1,14 @@
 package Login;
 
 public class Problem {
+
     public static void main(String[] args) {
+
         for (int j = 0; j < 10; j++) {
             System.out.println(j);
 
         }
+
         //some code here master
         //another code here master
 
